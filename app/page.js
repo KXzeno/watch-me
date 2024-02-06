84add6c238aeb3d34bcde650e2e22239b67a6e07
git <82054121+KXzeno@users.noreply.github.com>
2024-02-06 13:55:20 -0800

    feat: add randomized prompt for input
    Takes an array of fixed prompts which outputs "randomly" to the initial
    render

--- app/page.js
+++ app/page.js
@@ -3,13 +3,21 @@ import Image from "next/image";
 
 export default function Home() {
   let handleClick = () => alert('Who?');
+  let initialPrompt = [
+    "What brings greater company than most?",
+    "For what do we have an infinite appetite for?",
+    "In becoming greater, who do you make your prized witness?",
+  ]
+
+  let promptRandom = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <form onSubmit={handleClick}>
-        <input 
-          className="text-black"
-        />
+        <p className="text-center mb-2">{promptRandom(initialPrompt)}</p>
+        <div className="flex justify-center">
+          <input className="text-black" />
+        </div>
       </form>
     </main>
   );
