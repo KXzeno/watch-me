6250d5567e1848d28087b11e8fdb65d89bc19eca
git <82054121+KXzeno@users.noreply.github.com>
2024-02-08 09:07:39 -0800

    refactor: modularize initial prompts

---
+++ app/components/prompts/initialPrompts.jsx
@@ -0,0 +1,20 @@
+/* Initial prompt renderer */
+const initialPrompts = [
+  {
+    id: 1,
+    msg: "What brings greater company than most?",
+    ans: "silence",
+  },
+  {
+    id: 2,
+    msg: "For what do we have an infinite appetite for?",
+    ans: "distraction",
+  },
+  {
+    id: 3,
+    msg: "In becoming greater, who do you make your prized witness?",
+    ans: "myself",
+  },
+]
+
+export default initialPrompts;
